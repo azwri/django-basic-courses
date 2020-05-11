b1110136b01543a68f8d103c24770f8bb08f7451
azwri <aazwri@gmail.com>
2020-05-12 02:23:23 +0300

    add blog-> Post model, views, urls and templates

--- olx/blog/views.py
+++ olx/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+  all_posts = Post.objects.all()
+  return render(request, 'blog/all_posts.html', {'posts':all_posts})
+
+def post_detail(request, id):
+  post_detail = Post.objects.get(id=id)
+  return render(request, 'blog/post_detail.html', {'post':post_detail})
